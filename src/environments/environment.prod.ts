a38440af643b28c694d972db9b6be6f967b5b2fe
froz-ferre <alexej.ferre@gmail.com>
2018-11-12 22:17:13 +0200

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCJMy6bkMBeORQ-4z8mY23TLUoY54n9jhw',
+    authDomain: 'playful-adb84.firebaseapp.com',
+    databaseURL: 'https://playful-adb84.firebaseio.com',
+    projectId: 'playful-adb84',
+    storageBucket: 'playful-adb84.appspot.com',
+    messagingSenderId: '408681826999'
+  },
+
   production: true
 };
